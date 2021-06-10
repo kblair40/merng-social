46d9aa7df7ed18a2edf1fc950da6e16b0abb7e15
Kevin Blair <kblair40@gmail.com>
2021-06-10 16:10:06 -0500

    Add db connection

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("config.js");
 
 const typeDefs = gql`
   type Query {
@@ -18,6 +21,11 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server running at ${res.url}`);
-});
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
