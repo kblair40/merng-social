537816f1e141a681267676fd2834d1e238581d8c
Kevin Blair <kblair40@gmail.com>
2021-06-10 16:38:49 -0500

    add getPosts query

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const Post = require("./models/Post");
 const { MONGODB } = require("./config.js");
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    test: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    test: () => "success",
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
   },
 };
 
@@ -22,7 +36,7 @@ const server = new ApolloServer({
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     return server.listen({ port: 5000 });
   })
